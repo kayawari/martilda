bee75702dfc76c76651283c91eb6721601ed9ab9
takayuki.kayawari <kayamelo@gmail.com>
2018-12-08 17:43:30 +0900

    マークダウンのコンポーネントにvue-routerのルートを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Editor from '@/components/Editor'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/martilda',
+      name: 'martilda',
+      component: Editor
     }
   ]
 })
