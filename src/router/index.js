da87713417e56a2773f076a514bcfc0bc03c7eda
takayuki.kayawari <kayamelo@gmail.com>
2018-12-18 23:49:21 +0900

    remove editor route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Editor from '@/components/Editor'
 
 Vue.use(Router)
 
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/martilda',
-      name: 'martilda',
-      component: Editor
     }
   ]
 })
