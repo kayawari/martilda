42c5574286d0db7da897992ca70dd362846c7fc0
takayuki.kayawari <kayamelo@gmail.com>
2019-03-26 23:14:12 +0900

    Add settings for vue-bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
   mode: 'history',
