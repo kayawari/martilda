a59ec053a49607531c1ee188ce7dbc3c613e5a0d
takayuki.kayawari <kayamelo@gmail.com>
2019-06-16 17:26:23 +0900

    Add home components.

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='home'>
+    <img src="/static/leon.png">
+    <ul class="menu">
+      <router-link to="/signin" tag="li">Singin</router-link>
+      <router-link to="/editor" tag="li">Editor</router-link>
+    </ul>
+  </div>
+</template>
+<style lang='scss' scoped>
+  .menu {
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    border-bottom: 1px solid gray;
+    li {
+      padding: 10px 0;
+      border-top: 1px solid gray;
+      cursor: pointer;
+    }
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 import Signin from '@/components/Signin'
 import Editor from '@/components/Editor'
 import NotFound from '@/components/NotFound'
@@ -11,6 +12,11 @@ Vue.use(BootstrapVue)
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/signin',
       name: 'Signin',
